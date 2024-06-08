03c0b06339ad861a2dc8cbdeff7606be6662d642
qsmsoft <qsmsoft@gmial.com>
2024-06-08 11:24:46 +0500

    adding token verification

--- utils/jwt.go
+++ utils/jwt.go
@@ -9,7 +9,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GenerateToken(email string, userId int64) (string, error) {
+func secretKey() string {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file")
@@ -20,16 +20,50 @@ func GenerateToken(email string, userId int64) (string, error) {
 		log.Fatalf("SECRET_KEY environment variable not set")
 	}
 
+	return secretKey
+}
+
+func GenerateToken(email string, userId int64) (string, error) {
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
 
-	tokenString, err := token.SignedString([]byte(secretKey))
+	tokenString, err := token.SignedString([]byte(secretKey()))
 	if err != nil {
 		return "", err
 	}
 
 	return tokenString, nil
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+
+		return []byte(secretKey()), nil
+	})
+	if err != nil {
+		log.Fatalf("Could not parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+	if !tokenIsValid {
+		log.Fatalf("Token is not valid")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	// if !ok {
+	// 	log.Fatalf("Could not parse claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
